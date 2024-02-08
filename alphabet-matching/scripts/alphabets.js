d270d0778d9cbb7c8da57bf0200e37556cc5b537
Gru Singh <ontariosingh@gmail.com>
2024-02-07 21:43:19 -0500

    WIP

---
+++ alphabet-matching/scripts/alphabets.js
@@ -0,0 +1,177 @@
+const allAlphabets = [
+    {
+        alphabet: "ੳ",
+        images: ['camel']
+    },
+    {
+        alphabet: "ਅ",
+        images: ['eye']
+    },
+    {
+        alphabet: "ੲ",
+        images: ['engine']
+    },
+    {
+        alphabet: "ਸ",
+        images: ['apple']
+    },
+    {
+        alphabet: "ਹ",
+        images: ['elephant']
+    },
+    {
+        alphabet: "ਕ",
+        images: ['crow']
+    },
+    {
+        alphabet: "ਖ",
+        images: ['cucumber']
+    },
+    {
+        alphabet: "ਗ",
+        images: ['carrot']
+    },
+    {
+        alphabet: "ਘ",
+        images: ['horse']
+    },
+    {
+        alphabet: "ਙ",
+        images: ['flag']
+    },
+    {
+        alphabet: "ਚ",
+        images: ['rat']
+    },
+    {
+        alphabet: "ਛ",
+        images: ['umbrella']
+    },
+    {
+        alphabet: "ਜ",
+        images: ['airplane']
+    },
+    {
+        alphabet: "ਝ",
+        images: ['flag']
+    },
+    {
+        alphabet: "ਞ",
+        images: []
+    },
+    {
+        alphabet: "ਟ",
+        images: ['tomato']
+    },
+    {
+        alphabet: "ਠ",
+        images: ['scorpio']
+    },
+    {
+        alphabet: "ਡ",
+        images: ['frog']
+    },
+    {
+        alphabet: "ਢ",
+        images: ['camel']
+    }
+    ,
+    {
+        alphabet: "ਣ",
+        images: []
+    },
+    {
+        alphabet: "ਤ",
+        images: ['parrot']
+    },
+    {
+        alphabet: "ਥ",
+        images: ['carrybag']
+    },
+    {
+        alphabet: "ਦ",
+        images: ['tooth']
+    }
+    ,
+    {
+        alphabet: "ਧ",
+        images: ['earth']
+    }
+    ,
+    {
+        alphabet: "ਨ",
+        images: ['lemon']
+    },
+    {
+        alphabet: "ਪ",
+        images: ['kite']
+    },
+    {
+        alphabet: "ਫ",
+        images: ['flower']
+    }
+    ,
+    {
+        alphabet: "ਬ",
+        images: ['schoolbus']
+    },
+    {
+        alphabet: "ਭ",
+        images: ['bear']
+    },
+    {
+        alphabet: "ਮ",
+        images: ['fish']
+    },
+    {
+        alphabet: "ਯ",
+        images: ['chariot']
+    },
+    {
+        alphabet: "ਰ",
+        images: ['roti']
+    },
+    {
+        alphabet: "ਲ",
+        images: ['toytop']
+    },
+    {
+        alphabet: "ਵ",
+        images: ['velna']
+    },
+    {
+        alphabet: "ੜ",
+        images: ['rooster']
+    },
+    {
+        alphabet: "ਸ਼",
+        images: []
+    },
+    {
+        alphabet: "ਖ਼",
+        images: ['rabbit']
+    },
+    {
+        alphabet: "ਗ਼",
+        images: ['balloon']
+    },
+    {
+        alphabet: "ਜ਼",
+        images: []
+    },
+    {
+        alphabet: "ਫ਼",
+        images: []
+    },
+];
+
+const quizAlphabets = allAlphabets
+    .filter(a => a.images.length > 0)
+    .map(a => a.alphabet);
+
+export function getAlphabetImageSrc(alphabet) {
+    const result = allAlphabets.find(a => a.alphabet === alphabet);
+    return result ? `images/${result.images[0]}.webp` : '';
+}
+
+export default quizAlphabets;
---
+++ alphabet-matching/scripts/controllers.js
@@ -0,0 +1,67 @@
+import alphabets, {getAlphabetImageSrc} from './alphabets.js';
+
+function pickFourRandomAlphabets(pastAlphabets) {
+    const remaining = alphabets.filter(a => !pastAlphabets.includes(a));
+    const choices = [];
+
+    for (let i = 0; i < 4; i++) {
+        const randomIndex = Math.floor(Math.random() * remaining.length);
+        const randomItem = remaining[randomIndex];
+        choices.push(randomItem);
+        remaining.splice(randomIndex, 1);
+    }
+
+    return choices;
+}
+
+function selectRandomAlphabet(choices) {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+export function gameController() {
+    return {
+        score: 0,
+        pastAlphabets: [],
+        alphabet: null,
+        alphabetImageSrc: '',
+        flipped: [],
+        choices: [],
+        init() {
+            this.nextSelection();
+        },
+        nextSelection() {
+            if (this.alphabet) {
+                this.pastAlphabets.push(this.alphabet);
+            }
+
+            const choices = pickFourRandomAlphabets(this.pastAlphabets);
+            const alphabet = selectRandomAlphabet(choices);
+
+            this.flipped = [];
+            this.alphabet = alphabet;
+            this.alphabetImageSrc = getAlphabetImageSrc(alphabet);
+            this.choices = choices;
+
+            console.log('***nextSelection: ', this.alphabet, this.alphabetImageSrc, this.choices);
+        },
+        onChoice(alphabet) {
+            this.flipped.push(alphabet);
+            const correct = alphabet === this.alphabet;
+            if (correct || this.flipped.length >= 4) {
+                setTimeout(() => {
+                    this.nextSelection();
+                }, 1000);
+            }
+        },
+        hasNotFlipped(alphabet) {
+            return !this.flipped.includes(alphabet);
+        },
+        hasFlippedCorrectly(alphabet) {
+            return this.flipped.includes(alphabet) && alphabet === this.alphabet;
+        },
+        hasFlippedIncorrectly(alphabet) {
+            return this.flipped.includes(alphabet) && alphabet !== this.alphabet;
+        }
+    }
+}
