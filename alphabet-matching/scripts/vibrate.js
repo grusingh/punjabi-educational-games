4d746afd7a60c151d38214dbe571bf45d9bfa037
Gru Singh <ontariosingh@gmail.com>
2024-02-19 19:35:25 -0500

    Add audio feedback

--- alphabet-matching/scripts/vibrate.js
+++ alphabet-matching/scripts/vibrate.js
@@ -1,8 +1,30 @@
+function playAudio(audio) {
+    // If the audio is playing, stop it, and reset the playback position
+    if (!audio.paused) {
+        audio.pause();
+        audio.currentTime = 0;
+    } else {
+        // Play the audio if it is not already playing
+        audio.play();
+    }
+}
+
+function playCorrectChoiceAudio() {
+    const audio = document.getElementById('dhol-beat-audio');
+    playAudio(audio);
+}
+
+function playWrongChoiceAudio() {
+    const audio = document.getElementById('soft-warning-buzzer');
+    playAudio(audio);
+}
+
 export function vibrateCorrect() {
     // Vibrate with a pattern: several short bursts to mimic the feeling of confetti
-    if (window.navigator.vibrate) {
+    if (!window.navigator.vibrate) {
         navigator.vibrate([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]);
     }
+    playCorrectChoiceAudio();
 }
 
 export function vibrateWrong() {
@@ -10,4 +32,5 @@ export function vibrateWrong() {
     if (window.navigator.vibrate) {
         navigator.vibrate(500);
     }
+    playWrongChoiceAudio();
 }
